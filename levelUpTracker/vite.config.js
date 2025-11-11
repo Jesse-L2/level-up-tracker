907efdc170fad9e8f4abc387c918d05993c46cf6
Jesse-L2 <jesse.little@utexas.edu>
2025-11-11 11:21:38 -0600

    Fix vite config

--- levelUpTracker/vite.config.js
+++ levelUpTracker/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // or host: '0.0.0.0'
+    port: 5173,
+  },
+  // ... rest of your config
+});
